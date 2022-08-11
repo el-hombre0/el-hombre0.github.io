91a0bf764f13ca4b059477f5d5c05c65a63fc597
Stanislav E <evendot@yandex.ru>
2022-08-12 02:08:08 +0300

    Made the dynamic news pages

--- src/main/java/com/example/highstyle/controller/MainController.java
+++ src/main/java/com/example/highstyle/controller/MainController.java
@@ -1,13 +1,41 @@
 package com.example.highstyle.controller;
 
+import com.example.highstyle.model.News;
+import com.example.highstyle.repository.NewsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class MainController {
-    @GetMapping
+    @Autowired
+    NewsRepository newsRepository;
+    @GetMapping("/")
     public String index(Model model){
+        Iterable<News> news = newsRepository.findAll();
+        model.addAttribute("news", news);
         return "index";
+    }
+
+    @GetMapping("news")
+    public String news(@RequestParam("id") Integer id, Model model){
+        Optional<News> news_ind = newsRepository.findById(id);
+        news_ind.ifPresent(o -> model.addAttribute("news_ind", o));
+        return "news";
+    }
+
+    @GetMapping("historical")
+    public String historical(Model model){
+        return "historical";
+    }
+
+    @GetMapping("classification")
+    public String classification(Model model){
+        return "classification";
     }
 }
